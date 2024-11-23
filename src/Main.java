452bdc03e63b7def303b7ff7d136d73e4a002a11
Anton199929 <antonmayorov999@gmail.com>
2024-11-23 18:33:04 +0700

    hw12

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,16 @@
 public class Main {
     public static void main(String[] args) {
 
+        Author bookAuthor1 = new Author("Стивен", "Кинг");
+        Author bookAuthor2 = new Author("Глен", "Кук");
+        Book book1 = new Book("Оно", bookAuthor1, 1986);
+        Book book2 = new Book("Черный отряд", bookAuthor2, 1983);
+
+        System.out.println(book1);
+        System.out.println(book2);
+
+        book2.setYearOfPublication(1984);
+        System.out.println(book2);
+
     }
 }
